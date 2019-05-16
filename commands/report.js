1fd722cda4f0a747596ba09684a065e4d7a4c1dc
SamDoesMC <50482368+SamDoesMC@users.noreply.github.com>
2019-05-16 14:44:24 +0800

    Add files via upload

---
+++ commands/report.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const botconfig = require("../config.json");
+const black = botconfig.black;
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    if(args[0] == "help"){
+      message.reply("Usage: !report <user> <reason>");
+      return;
+    }
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return errors.cantfindUser(message.channel);
+    let rreason = args.join(" ").slice(22);
+    if(!rreason) return errors.noReason(message.channel);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor(black)
+    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "log");
+    if(!reportschannel) return message.channel.send("Couldn't find log channel.");
+    reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name: "report"
+}
